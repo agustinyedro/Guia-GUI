96cd0b1bb5d19442a7d88ba0eebde5aed458db9a
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2023-08-15 16:44:07 -0300

    Add files via upload

---
+++ GUI4-1/src/gui4/pkg1/Productos.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gui4.pkg1;
+
+/**
+ *
+ * @author USURIO
+ */
+public class Productos {
+    private int codigo;
+    private String Descripcion;
+    private double precio;
+    private int stock;
+    private String rubro;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(String rubro) {
+        this.rubro = rubro;
+    }
+    
+}
